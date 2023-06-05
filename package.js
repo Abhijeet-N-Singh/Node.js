6efff16e5172341b94d0c70db2d47f998556f287
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-05 12:36:29 +0530

    Create package.js
    In order to create our first REST resource,  we will use the express package.
    
    In Node.js, express is our most trustworthy friend. Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "myapp",
+  "version": "1.0.0",
+  "description": "this is my first Node.js Project\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[D\u001b[C\u001b[C\u001b[C\u001b[C\u001b[T",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node index.js"
+  },
+  "author": "Abhijeet",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
