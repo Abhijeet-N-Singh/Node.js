b9a7d4d70eaa8433237789ff85c4abeaf09efd1d
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-14 01:44:50 +0530

    spread.js
    spread operator

---
+++ spread.js
@@ -0,0 +1,10 @@
+function newFunction () {
+    const array1 = ["1","2","3","4"] ;
+    
+    const array2 = [...array1,"5","6","7","8"] ;
+  //... -> spread operator
+
+    console.log("Array: ",array2) ;
+}
+
+newFunction()
